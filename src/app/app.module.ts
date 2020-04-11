43d7c2212a1cc7bb4d5644d22b4a8fec88ea197d
Gabrielle Reese <gabrielle@s56.net>
2020-04-11 10:40:15 -0600

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { GoalsComponent } from './goals/goals.component';
+import { DailygoalsComponent } from './dailygoals/dailygoals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SigninComponent,
+    SignupComponent,
+    GoalsComponent,
+    DailygoalsComponent
   ],
   imports: [
     BrowserModule,
